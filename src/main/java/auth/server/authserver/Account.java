39c0cf614041ff7149b9215d3243a6c3f4c74681
GaluskaVlad1 <vlad.vvlad122@gmail.com>
2020-05-28 14:14:04 +0300

    Authentification server

---
+++ src/main/java/auth/server/authserver/Account.java
@@ -0,0 +1,20 @@
+package auth.server.authserver;
+
+import org.springframework.data.annotation.Id;
+
+public class Account {
+    @Id
+    public String id;
+
+    public String username;
+    public String password;
+
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String toString(){
+        return username+","+password;
+    }
+}
